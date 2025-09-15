2282c3ef6b968795c81a0572b159177a9b293539
ArthurM0811 <arthurmanoel825@gmail.com>
2025-09-15 19:11:08 -0300

    Update clientesControllers.js

--- controllers/clientes/clientesControllers.js
+++ controllers/clientes/clientesControllers.js
@@ -14,7 +14,7 @@ const clientes = (req, res) => {
     });
 };
 
-const cadastrarclientes = (req, res) => {
+const cadastrarcliente = (req, res) => {
 
     const { Nome, Fone, CPF } = req.body;
     const userId = req.user.id;
@@ -30,4 +30,46 @@ const cadastrarclientes = (req, res) => {
 
 }
 
-module.exports = { clientes, cadastrarclientes };
+const excluircliente = (req, res) => {
+
+    const id = req.params.id;
+
+    sql = "DELETE FROM Cliente WHERE idCliente = ?";
+
+    db.query(sql, [id], (err, result) => {
+
+        if (err) return res.status(500).json({error: "Erro no servidor"});
+
+        if (result.affectedRows === 0) return res.status(404).json({error: "Cliente não encontrado"});
+
+        res.json("Cliente excluido com sucesso!");
+
+    });
+
+};
+
+const atualizarcliente = (req, res) => {
+
+    const { Nome, Fone, CPF } = req.body;
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    const sql = `
+        UPDATE Cliente
+        SET Nome = ?, Fone = ?, CPF = ?
+        WHERE idCliente = ? AND Usuario_idUsuario = ?
+    `;
+
+    db.query(sql, [Nome, Fone, CPF, id, userId], (err, result) => {
+
+        if (err) return res.status(500).json({error: "Erro no servidor"});
+
+        if (result.affectedRows === 0) return res.status(500).json({error: "Erro no servidor"});
+
+        res.json("Cliente atualizado com sucesso!");
+
+    });
+
+};
+
+module.exports = { clientes, cadastrarcliente, excluircliente, atualizarcliente };
