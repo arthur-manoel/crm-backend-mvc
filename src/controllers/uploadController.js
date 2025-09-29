05701936f5c9406532e77e3a6bc6eb2bd7dfde01
ArthurM0811 <arthurmanoel825@gmail.com>
2025-09-29 09:29:47 -0300

    Modificar a rota de upload para salvar informações no banco de documentos

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,27 +1,48 @@
 import multer from "multer";
 import path from "path";
+import db from "../database/db.js"
 
-const uploadArquivos = (req, res, next) => {
 
-    const storage = multer.diskStorage({
+const storage = multer.diskStorage({
+    
+    destination: (req, file, cb) => {
+        
+        cb(null, "uploads");
+        
+    },
 
-        destination: (req, file, cb) => {
-            cb(null, "uploads");
-        },
-
-        filename: (req, file, cb) => {
-            const identificador = Date.now() + "-" + Math.round(Math.random() * 1e9);
-            cb(null, identificador + path.extname(file.originalname));
-        }
+    filename: (req, file, cb) => {
+        const identificador = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, identificador + path.extname(file.originalname));
+    }
 
     });
 
-    const upload = multer({ storage }).single("arquivo");
+const upload = multer({ storage }).single("arquivo");
 
-    upload(req, res, (err) => {
+const uploadArquivos = (req, res, next) => {
+
+    upload(req, res, async (err) => {
+
         if (err) return res.status(500).send("Erro ao enviar arquivo! " + err.message);
         if (!req.file) return res.status(400).send("Arquivo não encontrado");
-        res.send("Arquivo enviado com sucesso!");
+        
+        try {
+
+            const link = `uploads/${req.file.filename}`;
+            const { cliente_id } = req.params;
+
+            const { tipo_documento_id, cnpj_id } = req.body;
+
+            const sql = "INSERT INTO documento (link, tipo_documento_id, cliente_id, cnpj_id) VALUES (?, ?, ?, ?)";
+
+            const [rows] = await db.execute(sql, [link, tipo_documento_id, cliente_id, cnpj_id]);
+            
+            res.send("arquivo enviado com sucesso!")
+
+        } catch (error) {
+            res.status(500).json({message: error});
+        }
     });
 
 };
--- src/routes/uploadRoute.js
+++ src/routes/uploadRoute.js
@@ -3,6 +3,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/uploadarquivo", uploadArquivos);
+router.post("/uploadarquivo/:cliente_id", uploadArquivos);
 
 export default router
