5abda819c99507afcf06e49c18966e8c44416625
ArthurM0811 <arthurmanoel825@gmail.com>
2025-09-30 08:49:08 -0300

    nova rota de tiposProcesso e atualiza dashboard

--- src/controllers/dashboardControllers.js
+++ src/controllers/dashboardControllers.js
@@ -5,17 +5,47 @@ const dashboard = async (req, res) => {
     const sql = `
       SELECT
         (SELECT COUNT(*) FROM cliente) AS totalClientes, 
-        (SELECT COUNT(*) FROM cnpj) AS totalCNPJS
+        (SELECT COUNT(*) FROM cnpj) AS totalCNPJS,
+        (SELECT SUM(ativo) FROM cnpj_status) AS totalAtivos,
+        (SELECT SUM(pendente) FROM cnpj_status) AS totalPendentes
     `;
     const [rows] = await db.query(sql);
+
     if (!rows || rows.length === 0) {
-      return res.status(200).json({ totalClientes: 0, totalCNPJS: 0 });
+      return res.status(200).json({ totalClientes: 0, totalCNPJS: 0, totalAtivos: 0, totalPendentes: 0 });
     }
-    const { totalClientes, totalCNPJS } = rows[0];
-    res.json({ totalClientes, totalCNPJS });
+    const { totalClientes, totalCNPJS, totalAtivos, totalPendentes } = rows[0];
+    
+    res.json({ totalClientes, totalCNPJS, totalAtivos, totalPendentes });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-export default dashboard;
+const tiposProcesso = async (req, res) => {
+
+  try {
+    const sql = `
+    SELECT
+        (SELECT COUNT(*) FROM tipo_processo WHERE tipo = "aberto") AS totalAbertos, 
+        (SELECT COUNT(*) FROM tipo_processo WHERE tipo = "alterados") AS totalAlterados,
+        (SELECT COUNT(*) FROM tipo_processo WHERE tipo = "fechados") AS totalFechados
+    `
+    const [rows] = await db.query(sql);
+    
+    if (!rows || rows.length == 0) {
+      return res.status(200).json({totalAberto: 0, totalAlterados: 0, totalFechados: 0})
+    }
+
+    const { totalAbertos, totalAlterados, totalFechados } = rows[0]
+
+    res.json({totalAbertos, totalAlterados, totalFechados})
+
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+
+}
+
+export { dashboard, tiposProcesso };
--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import dashboard from "../controllers/dashboardControllers.js";
+import { dashboard, tiposProcesso } from "../controllers/dashboardControllers.js";
 import { VerificarToken } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.get("/dashboard", VerificarToken, dashboard);
+router.get("/tiposProcesso", VerificarToken, tiposProcesso)
 
 export default router;
